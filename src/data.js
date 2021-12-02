477d5c677a625fbf416ca61c8c1435a28b02832f
ADE <adejumoahmad4life@gmail.com>
2021-12-02 14:44:33 +0100

    added more image cards

--- src/data.js
+++ src/data.js
@@ -9,4 +9,24 @@ export const products = [
     img: "https://www.linkpicture.com/q/screencapture-ahmad-blog-herokuapp-2021-12-02-14_23_04.png",
     link: "https://ahmad-blog.herokuapp.com/",
   },
+  {
+    id: 3,
+    img: "https://www.linkpicture.com/q/gpt3.png",
+    link: "https://gpt3-nft.netlify.app/",
+  },
+  {
+    id: 4,
+    img: "https://www.linkpicture.com/q/screencapture-ahmad-blog-herokuapp-2021-12-02-14_23_04.png",
+    link: "https://ahmad-blog.herokuapp.com/",
+  },
+  {
+    id: 5,
+    img: "https://www.linkpicture.com/q/gpt3.png",
+    link: "https://gpt3-nft.netlify.app/",
+  },
+  {
+    id: 6,
+    img: "https://www.linkpicture.com/q/screencapture-ahmad-blog-herokuapp-2021-12-02-14_23_04.png",
+    link: "https://ahmad-blog.herokuapp.com/",
+  },
 ];
