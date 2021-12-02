63732af08b7e2696fa4fcb8ad77fb1ce6e9323bf
ADE <adejumoahmad4life@gmail.com>
2021-12-02 14:38:54 +0100

    fixed a bug

--- src/data.js
+++ src/data.js
@@ -1,14 +1,12 @@
-import gpt3 from "./img/gpt3.png";
-import blog from "./img/blog.png";
 export const products = [
   {
     id: 1,
-    img: { gpt3 },
+    img: "https://www.linkpicture.com/q/gpt3.png",
     link: "https://gpt3-nft.netlify.app/",
   },
   {
     id: 2,
-    img: { blog },
+    img: "https://www.linkpicture.com/q/screencapture-ahmad-blog-herokuapp-2021-12-02-14_23_04.png",
     link: "https://ahmad-blog.herokuapp.com/",
   },
 ];
